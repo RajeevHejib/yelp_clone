39cd32807c2e9ed6cf1b77831e66646ffc127c4a
Rajeev Hejib <rajeev_hejib@yahoo.com>
2015-11-28 17:00:30 +0000

    can upload images now

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'devise'
 gem 'omniauth-facebook'
+gem 'paperclip'
+gem 'aws-sdk', '< 2.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,10 @@ class Restaurant < ActiveRecord::Base
   validates :name, length: {minimum: 3}, uniqueness: true
   belongs_to :user
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+
   def average_rating
     return "N/A" if reviews.none?
     # reviews.inject(0) {|memo, review| memo + review.rating} / reviews.count
@@ -15,5 +19,6 @@ class Restaurant < ActiveRecord::Base
     review.user = user
     review
   end
+
 
 end
